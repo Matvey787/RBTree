9cc06012142f8577af86c2cd2bdf04e515f58569
Matvey787 <matveyklg@gmail.com>
2025-10-01 22:10:43 +0300

    -  Do not use erase, as it deletes elements as in a regular binary tree. IT CAN SIMPLY BREAK EVERYTHING. - Added support for searching the distance between elements (range_query).

--- inc/RBT.hpp
+++ inc/RBT.hpp
@@ -35,16 +35,35 @@ public:
 
 template <typename KeyT, typename Comp = std::less<KeyT>>
 class SearchTree {
-
-    using iterator = Node<KeyT>*;
     Node<KeyT> *top_ = nullptr;
 
     void balance(Node<KeyT> *node);
     void gdumpNode(std::ofstream &ofs, Node<KeyT> *node) const;
     void LLRot(Node<KeyT> *node);
     void RRRot(Node<KeyT> *node);
+    void BSTErase(KeyT key);
+    Node<KeyT>* findMinInSubtree(Node<KeyT> *subRoot) const;
 
 public:
+    Node<KeyT>* successor(Node<KeyT>* node) const {
+        if (!node) return nullptr;
+
+        if (node->right_) {
+            Node<KeyT>* cur = node->right_;
+            while (cur->left_) cur = cur->left_;
+            return cur;
+        }
+
+        Node<KeyT>* cur = node;
+        Node<KeyT>* p = cur->parent_;
+        while (p && cur == p->right_) {
+            cur = p;
+            p = p->parent_;
+        }
+        return p;
+    }
+    
+    using iterator = Node<KeyT>*;
     iterator lower_bound(KeyT key) const;
     iterator upper_bound(KeyT key) const;
     int distance(iterator fst, iterator snd) const;
@@ -95,6 +114,101 @@ void SearchTree<KeyT, Comp>::insert(KeyT key)
 }
 
 template <typename KeyT, typename Comp>
+Node<KeyT>* SearchTree<KeyT, Comp>::findMinInSubtree(Node<KeyT> *subRoot) const
+{
+    Node<KeyT> *minNode = subRoot;
+
+    while (minNode && minNode->left_) minNode = minNode->left_;
+
+    // смотрим что это не корень поддерева,
+    // иначе это означает что левых потомков в этом поддереве нет
+    if (minNode == subRoot) return nullptr;
+    return minNode;
+}
+
+template <typename KeyT, typename Comp>
+void SearchTree<KeyT, Comp>::BSTErase(KeyT key)
+{
+    
+    auto replace = [this](Node<KeyT>* oldNode, Node<KeyT>* newNode) {
+        if (!oldNode || !newNode) return;
+
+        if (!newNode->left_ && oldNode->left_) newNode->left_ = oldNode->left_;
+
+        newNode->parent_ = oldNode->parent_;
+        if (!oldNode->parent_)
+        {
+            top_ = newNode;
+        }
+        else
+        {
+            if (oldNode->parent_->left_ == oldNode)
+                oldNode->parent_->left_ = newNode;
+            else
+            {
+                oldNode->parent_->right_ = newNode;
+                std::cout << "here1111" << std::endl;
+            }
+        }
+        oldNode->left_ = nullptr;
+        oldNode->right_ = nullptr;
+        delete oldNode;
+    };
+
+    Node<KeyT> *deleted = top_;
+    while (deleted) // ищем узел для удаления
+    {
+        if (Comp{}(key, deleted->key_))
+            deleted = deleted->left_;
+        else if (Comp{}(deleted->key_, key))
+            deleted = deleted->right_;
+        else
+            break;
+    }
+
+    if (!deleted) return;
+
+    if(!deleted->left_ && !deleted->right_) // если это лист
+    {
+        if (deleted->parent_->left_ == deleted)
+            deleted->parent_->left_ = nullptr;
+        else
+            deleted->parent_->right_ = nullptr;
+
+        delete deleted;
+    }
+    else if (deleted->right_ && deleted->left_)
+    {
+        Node<KeyT> *inorderedSuccessor = findMinInSubtree(deleted->right_);
+
+        if (!inorderedSuccessor) // минимальный элемент в правом поддереве не найден
+        {
+            replace(deleted, deleted->right_);
+        }
+        else
+        {
+            replace(deleted, inorderedSuccessor);
+        }
+    }
+    else if (deleted->right_) // есть только правый потомок
+    {
+        std::cout << "Удаляется элемент с правым потомком " << deleted->key_ << std::endl;
+        replace(deleted, deleted->right_);
+        std::cout << "удалён" << std::endl;
+    }
+    else // есть только левый потомок
+    {
+        replace(deleted, deleted->left_);
+    }
+}
+
+template <typename KeyT, typename Comp>
+void SearchTree<KeyT, Comp>::erase(KeyT key)
+{
+    BSTErase(key);
+}
+
+template <typename KeyT, typename Comp>
 void SearchTree<KeyT, Comp>::LLRot(Node<KeyT> *node)
 {
     Node<KeyT> *p = node->parent_;
@@ -108,9 +222,10 @@ void SearchTree<KeyT, Comp>::LLRot(Node<KeyT> *node)
             g->parent_->right_ = p;
             
         g->left_ = p->right_;
+        g->parent_ = p;
         p->right_ = g;
-        p->color_ = color_t::BLACK;
-        g->color_ = color_t::RED;
+
+        std::swap(p->color_, g->color_);
     }
 }
 
@@ -128,9 +243,9 @@ void SearchTree<KeyT, Comp>::RRRot(Node<KeyT> *node)
             g->parent_->right_ = p;
             
         g->right_ = p->left_;
+        g->parent_ = p;
         p->left_ = g;
-        p->color_ = color_t::BLACK;
-        g->color_ = color_t::RED;
+        std::swap(p->color_, g->color_);
     }
 }
 
@@ -205,6 +320,7 @@ void SearchTree<KeyT, Comp>::balance(Node<KeyT> *node)
 template <typename KeyT, typename Comp>
 void SearchTree<KeyT, Comp>::gdump() const
 {
+    std::cout << "графическое представление дерева" << std::endl;
     static int counter = 0;
     std::string graphviz_file = "graphviz/tree" + std::to_string(counter) + ".dot";
     std::ofstream ofs(graphviz_file);
@@ -214,6 +330,7 @@ void SearchTree<KeyT, Comp>::gdump() const
     gdumpNode(ofs, top_);
     ofs << "}\n";
     ofs.close();
+    std::cout << "графическое представление дерева" << std::endl;
 
     std::string dot_cmd = "dot -Tsvg graphviz/tree" + std::to_string(counter) + ".dot -o graphviz/tree" + std::to_string(counter + 1) + ".svg";
     system(dot_cmd.c_str());
@@ -227,12 +344,14 @@ void SearchTree<KeyT, Comp>::gdumpNode(std::ofstream &ofs, Node<KeyT> *node) con
 {
     if (!node) return;
 
+    std::cout << "Print node " << node->key_ << std::endl;
+
     std::string color_attr = (node->color_ == color_t::RED) ? 
         "fillcolor=lightcoral, fontcolor=white" : 
         "fillcolor=lightgray, fontcolor=black";
 
     ofs << "    \"" << node << "\" [label=\"" << node->key_ << "\", " << color_attr << "];\n";
-    
+    std::cout << "gdumpNode    " << (node->parent_ ? node->parent_->key_ : -1) << std::endl;
     if (node->left_) {
         ofs << "    \"" << node << "\" -> \"" << node->left_ << "\";\n";
         gdumpNode(ofs, node->left_);
@@ -241,6 +360,63 @@ void SearchTree<KeyT, Comp>::gdumpNode(std::ofstream &ofs, Node<KeyT> *node) con
         ofs << "    \"" << node << "\" -> \"" << node->right_ << "\";\n";
         gdumpNode(ofs, node->right_);
     }
+
+    std::cout << "here2222" << std::endl;
+}
+
+template <typename KeyT, typename Comp>
+typename SearchTree<KeyT, Comp>::iterator 
+SearchTree<KeyT, Comp>::lower_bound(KeyT key) const {
+    Node<KeyT>* res = nullptr;
+    Node<KeyT>* cur = top_;
+    while (cur) {
+        if (!Comp{}(cur->key_, key)) { // cur->key_ >= key
+            res = cur;
+            cur = cur->left_;
+        } else {
+            cur = cur->right_;
+        }
+    }
+    return res;
+}
+
+template <typename KeyT, typename Comp>
+typename SearchTree<KeyT, Comp>::iterator 
+SearchTree<KeyT, Comp>::upper_bound(KeyT key) const {
+    Node<KeyT>* res = nullptr;
+    Node<KeyT>* cur = top_;
+    while (cur) {
+        if (Comp{}(key, cur->key_)) { // cur->key_ > key
+            res = cur;
+            cur = cur->left_;
+        } else {
+            cur = cur->right_;
+        }
+    }
+    return res;
+}
+
+
+template <typename C>
+int mydistance(const C& tree, typename C::iterator fst, typename C::iterator snd) {
+    int cnt = 0;
+    auto cur = fst;
+    while (cur && cur != snd) {
+        ++cnt;
+        cur = tree.successor(cur);
+    }
+    return cnt;
+}
+
+template <typename C, typename T>
+int range_query(const C& s, T fst, T snd)
+{
+    using itt = typename C::iterator;
+    itt start = s.lower_bound(fst);
+    // first not less then fst
+    itt fin = s.upper_bound(snd);
+    // first greater then snd
+    return mydistance(s, start, fin); // std::distance для set
 }
 
 } // namespace RBT
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,50 @@ int main()
     try
     {
         RBT::SearchTree<int> tree;
-        tree.insert(50);
-        tree.insert(30);
-        tree.gdump();
-        tree.insert(70);
-        tree.insert(20);
-        tree.insert(40);
+        // tree.insert(50);
+        // tree.insert(30);
+        // tree.gdump();
+        // tree.insert(70);
+        // tree.insert(20);
+        // tree.insert(40);
 
-        tree.gdump();
-        tree.insert(45);
-        tree.gdump();
-        tree.insert(10);
-        tree.gdump();
-        tree.insert(5);
-        tree.gdump();
+        // tree.gdump();
+        // tree.insert(45);
+        // tree.gdump();
+        // tree.insert(10);
+        // tree.gdump();
+        // tree.insert(5);
+        // tree.gdump();
+
+        // FIXME НЕ ВЫЗЫВАЙТЕ УДАЛЕНИЕ, ОНО РАБОТАЕТ БЕЗ БАЛАНСИРОВКИ, ЭТО УДАЛЕНИЕ В SBT, А НЕ В 
+        // RBT !!!!!
+        // // tree.erase(30); 
+
+        // tree.gdump();
+
+        std::vector<int> results;
+        char command;
+        do {
+            std::cin >> command;
+            if (command == 'k')
+            {
+                int key = 0;
+                std::cin >> key;
+                tree.insert(key);
+            }
+            else if (command == 'q')
+            {
+                int key1 = 0; 
+                int key2 = 0;
+                std::cin >> key1 >> key2;
+                results.push_back(RBT::range_query(tree, key1, key2));
+            }
+            // tree.erase(30);
+        } while (command != 'o');
+
+        for (int result : results) {
+            std::cout << result << " " << std::endl;
+        }
     }
 
     catch (const std::exception &e)
